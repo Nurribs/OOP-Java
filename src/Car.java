9988a058277b5486d8973b97560879176d46b573
Nurribs <ayseibis50@gmail.com>
2022-10-14 14:43:09 +0300

    Object

--- src/Car.java
+++ src/Car.java
@@ -1,28 +1,47 @@
 class Car {
     //Nitelikler
-    String type= "Sedan";
+
+    String type;
     String model;
     String color;
     int speed;
-    int speedLimit=180;
+    int speedLimit;
 
     //Davranışlar
 
-    int increaseSpeed(int increment){
-        if (speed + increment < speedLimit){
-            speed += increment;
-        }
-        return speed;
+    Car(String model, int speed, String color) {//veri tipi sınıfta nasıl girildiyse burda da öyle girilmesi gerekir.
+        System.out.println("Nesne oluşturuldu.");
+        this.model = model;
+        this.speed = speed;
+        this.color = "Blue";
+        this.type = "Sedan";
+        this.speedLimit = 180;
+        System.out.println(this.model); // ekrana ilk önce metot(constructor(yapıcı) metot) içini yazdırır.
     }
 
-    int decreaseSpeed(int decrease){
-        if (speed>0){
-            speed -= decrease;
+    void increaseSpeed(int increment) {
+        if (this.speed + increment < this.speedLimit) {
+            this.speed += increment;
         }
-        return speed;
     }
 
-    void printSpeed(){
-        System.out.println("Speed: " +speed);
+    void decreaseSpeed(int decrease) {
+        if (this.speed > 0) {
+            this.speed -= decrease;
+        }
+    }
+
+    void printSpeed() {
+        System.out.println("Speed: " + this.speed);
+    }
+
+
+    void printInfo() {
+        System.out.println("Model\t:" +this.model);
+        System.out.println("Color\t:" +this.color);
+        System.out.println("Type\t:" +this.type);
+        System.out.println("Speed\t:" +this.speed);
+
     }
 }
+
